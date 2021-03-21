3bb251a75222939c943c4609f92118c4b589b06c
SpeedSter <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-03-21 13:13:40 -0400

    Create lol.cs

---
+++ lol.cs
@@ -0,0 +1,1 @@
+			MessageBox.Show("The Current Roblox Version is " + new WebClient().DownloadString("https://setup.rbxcdn.com/version"), "Roblox Version", MessageBoxButtons.OK, MessageBoxIcon.Information);
